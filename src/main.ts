620a18585a740a0c5d8bcc877361838c08aa5195
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2025-08-19 23:18:18 +0800

    fix: unexported utils

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 import mutexSingleQueue from "./mutexSingleQueue";
+import monacoInstance, { Range, KeyCode, KeyMod } from "./monacoForXyyWiki";
 
 export { mutexSingleQueue };
+export { monacoInstance, Range, KeyCode, KeyMod };
